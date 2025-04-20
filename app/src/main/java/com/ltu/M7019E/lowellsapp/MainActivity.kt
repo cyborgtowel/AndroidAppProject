d770524e5a36bfb5b7fc189569ff7abba636fc92
cyborgtowel <tan.lowell2001@gmail.com>
2025-04-20 18:53:51 +0200

    MovieNavigatorApp
    Signed-off-by: cyborgtowel <tan.lowell2001@gmail.com>

--- app/src/main/java/com/ltu/M7019E/lowellsapp/MainActivity.kt
+++ app/src/main/java/com/ltu/M7019E/lowellsapp/MainActivity.kt
@@ -3,34 +3,12 @@ package com.ltu.M7019E.lowellsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Scaffold
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ltu.M7019E.lowellsapp.model.Movie
-import com.ltu.M7019E.lowellsapp.database.Movies
 import com.ltu.M7019E.lowellsapp.ui.theme.LowellsAppTheme
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextOverflow
-import com.ltu.M7019E.lowellsapp.utils.Constants
-import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
